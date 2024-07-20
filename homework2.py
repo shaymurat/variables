fc0bfa5aa29a35a45772b4fac9e27fbfa22b49a9
shaymurat <shaymuratoff@ya.ru>
2024-07-21 02:53:32 +0500

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,9 @@
+completed_homework_count = 12
+hours_spent = 1.5
+course_name = 'Python'
+time_per_task = hours_spent / completed_homework_count
+print('Курс: ', course_name,
+    ', всего задач: ', completed_homework_count,
+    ', затрачено часов: ', hours_spent,
+    ', среднее время выполнения: ', time_per_task,
+    '.', sep='')
